1b78ffc03e14e3870d5d2efeb3c7cc53dced787a
Alexandra Soares <alexandra.soares@outlook.com>
2023-03-22 14:55:52 -0300

    feat: criar pensamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CriarPensamentosComponent } from './components/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { CriarPensamentosComponent } from './components/pensamentos/criar-pensam
     HomeComponent,
     CabecalhoComponent,
     RodapeComponent,
-    CriarPensamentosComponent
+    CriarPensamentosComponent,
+    ListarPensamentoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamentos',
   templateUrl: './criar-pensamentos.component.html',
   styleUrls: ['./criar-pensamentos.component.css']
 })
-export class CriarPensamentosComponent {
+export class CriarPensamentosComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert('epa!');
+  }
+
+  cancelar() {
+    alert('cancelou!');
+  }
 }
