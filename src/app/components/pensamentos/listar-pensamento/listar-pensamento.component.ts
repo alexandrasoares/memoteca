2a46e43df2018743802dcf8c189f1e4f70f9c88d
Alexandra Soares <alexandra.soares@outlook.com>
2023-03-22 16:39:46 -0300

    feat: adicionar pensamentos

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css']
 })
-export class ListarPensamentoComponent {
+export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamento = [
+    // {
+    //   conteudo: 'Teste Conteudo 1',
+    //   autoria: 'Alexandra',
+    //   modelo: 'modelo1',
+    // },
+    // {
+    //   conteudo: 'Teste Conteudo 2',
+    //   autoria: 'Alexandra',
+    //   modelo: 'modelo3',
+    // },
+    // {
+    //   conteudo: 'Teste Conteudo 3',
+    //   autoria: 'Alexandra',
+    //   modelo: 'modelo1',
+    // },
+    // {
+    //   conteudo: 'Teste Conteudo 4',
+    //   autoria: 'Alexandra',
+    //   modelo: 'modelo1',
+    // },
+    // {
+    //   conteudo: 'Teste Conteudo 5',
+    //   autoria: 'Alexandra',
+    //   modelo: 'modelo2',
+    // },
+    // {
+    //   conteudo: 'Teste Conteudo 6',
+    //   autoria: 'Alexandra',
+    //   modelo: 'modelo2',
+    // },
+    // {
+    //   conteudo: 'Teste Conteudo 7',
+    //   autoria: 'Alexandra',
+    //   modelo: 'modelo2',
+    // },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 }
